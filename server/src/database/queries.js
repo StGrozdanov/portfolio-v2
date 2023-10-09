2241d13ddc3870b4675bb74537dbcc9df218791f
Stoyan Grozdanov <stoyan@Stoyans-MacBook-Pro.local>
2023-10-09 12:22:11 +0300

    queries now return the additional user information

--- server/src/database/queries.js
+++ server/src/database/queries.js
@@ -1,18 +1,22 @@
 export const getFullUserInfoQuery = `SELECT email,
                                             cv_link                                       AS cvLink,
                                             about_me                                      AS aboutMe,
+                                            partners,
+                                            carousel,
                                             JSON_EXTRACT(technology_stack, '$.techStack') AS techStack,
                                             JSON_EXTRACT(soft_skills, '$.softSkills')     AS softSkills,
                                             JSON_EXTRACT(hobbies, '$.hobbies')            AS hobbies,
                                             social_media                                  AS socialMedia,
                                             jobs,
                                             projects
-                                    FROM users`;
+                                    FROM users;`;
 
 export const getBaseUserInfoQuery = `SELECT email,
                                             cv_link                                       AS cvLink,
-                                            about_me                                      AS aboutMe
-                                     FROM users;`;
+                                            about_me                                      AS aboutMe,
+                                            partners,
+                                            carousel
+                                    FROM users;`;
 
 export const getUserSkillsQuery = `SELECT JSON_EXTRACT(technology_stack, '$.techStack') AS techStack,
                                           JSON_EXTRACT(soft_skills, '$.softSkills')     AS softSkills,
