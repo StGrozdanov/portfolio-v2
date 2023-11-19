817f827764ccab469e66400f8c021cca6e7edd60
Stoyan Grozdanov <stoyan@Stoyans-MacBook-Pro.local>
2023-11-19 16:44:14 +0200

    implemented personal info CMS in the admin panel

--- client/src/components/Admin/modules/components/PersonalInfo/PersonalInfo.tsx
+++ client/src/components/Admin/modules/components/PersonalInfo/PersonalInfo.tsx
@@ -1,5 +1,64 @@
+import { faEnvelope, faFileArrowUp } from "@fortawesome/free-solid-svg-icons";
+import FileUpload from "../../../../FileUpload/FileUpload";
+import styles from './PersonalInfo.module.scss';
+import AnimatedIconInput from "../../../../AnimatedIconInput/AnimatedIconInput";
+import TextArea from "../../../../TextArea/TextArea";
+import ImageBoard from "../../../../ImageBoard/ImageBoard";
+import { usePersonalInfo } from "./hooks/usePersonalInfo";
+
 export default function PersonalInfo() {
+    const {
+        basicUserInfo,
+        isLoading,
+        updateAboutMeRequestHandler,
+        updateCarouselImagesHandler,
+        updateEmailRequestHandler,
+        updatePartnersHandler
+    } = usePersonalInfo();
+
     return (
-        <h1>Personal Info</h1>
+        isLoading
+            ? <span>Loading ...</span>
+            :
+            <article className={styles.container}>
+                <h1>Personal Info</h1>
+                <section className={styles['icon-section']}>
+                    <section className={styles['email-and-cv-section']}>
+                        <AnimatedIconInput
+                            fontAwesomeIcon={faEnvelope}
+                            defaultValue={basicUserInfo?.email}
+                            tip="Update Email"
+                            requestHandler={updateEmailRequestHandler}
+                        />
+                        <FileUpload
+                            fontAwesomeIcon={faFileArrowUp}
+                            tip="Upload your CV"
+                            uploadType="uploadCV"
+                            limited={false}
+                        />
+                    </section>
+                </section>
+                <TextArea
+                    label="About Me"
+                    name="about"
+                    defaultValue={basicUserInfo?.aboutMe}
+                    requestHandler={updateAboutMeRequestHandler}
+                />
+                <ImageBoard
+                    heading="Partners Banners"
+                    imageCollection={basicUserInfo ? basicUserInfo.partners : []}
+                    updateStateHandler={updatePartnersHandler}
+                    uploadType="addPartners"
+                    tip="Upload banner"
+                    limit={6}
+                />
+                <ImageBoard
+                    heading="Carousel Images"
+                    imageCollection={basicUserInfo ? basicUserInfo.carousel.map(carousel => carousel.imgURL) : []}
+                    updateStateHandler={updateCarouselImagesHandler}
+                    uploadType="addCarousel"
+                    tip="Upload Carousel"
+                />
+            </article>
     );
 }
---
+++ client/src/components/Admin/modules/components/PersonalInfo/hooks/usePersonalInfo.ts
@@ -0,0 +1,96 @@
+import { useEffect, useState } from "react";
+import { BasicInfoResponse } from "../../../../../../services/interfaces/portfolio-service-interfaces";
+import { useAuthContext } from "../../../../../../hooks/useAuthContext";
+import { portfolioAPI } from "../../../../../../services/portfolio-service";
+
+export const usePersonalInfo = () => {
+    const [basicUserInfo, setBasicUserInfo] = useState<BasicInfoResponse>();
+    const [isLoading, setIsLoading] = useState(true);
+    const { token } = useAuthContext();
+
+    useEffect(() => {
+        portfolioAPI
+            .getBasicUserInfo()
+            .then(response => {
+                setBasicUserInfo(response);
+                setIsLoading(false);
+            })
+            .catch(err => console.log(err));
+    }, []);
+
+    const updatePartnersHandler = (partners: string[]) => {
+        if (basicUserInfo) {
+            setBasicUserInfo((oldState) => {
+                portfolioAPI
+                    .updateBaseUserInfo({ ...basicUserInfo, partners, id: 1 }, token)
+                    .then(response => console.log(response))
+                    .catch(error => console.log(error));
+
+                return { ...oldState as BasicInfoResponse, partners }
+            });
+        }
+    }
+
+    const updateCarouselImagesHandler = (carouselImages: string[]) => {
+        if (basicUserInfo) {
+            setBasicUserInfo((oldState) => {
+                if (oldState) {
+                    const updatedCarousel = oldState.carousel.map((carousel, index) => ({
+                        ...carousel,
+                        imgURL: carouselImages[index],
+                    })).filter(carousel => carousel.imgURL);
+
+                    portfolioAPI
+                        .updateBaseUserInfo({ ...basicUserInfo, carousel: updatedCarousel, id: 1 }, token)
+                        .then(response => console.log(response))
+                        .catch(error => console.log(error));
+
+                    return { ...oldState as BasicInfoResponse, carousel: updatedCarousel }
+                }
+            });
+        }
+    }
+
+    const updateEmailRequestHandler = (email: string) => {
+        if (basicUserInfo) {
+            setBasicUserInfo((oldState) => {
+                if (!oldState) {
+                    return oldState;
+                }
+
+                portfolioAPI
+                    .updateBaseUserInfo({ ...basicUserInfo, email, id: 1 }, token)
+                    .then(response => console.log(response))
+                    .catch(error => console.log(error));
+
+                return { ...oldState, email }
+            });
+        }
+    }
+
+    const updateAboutMeRequestHandler = (aboutMe: string) => {
+        if (basicUserInfo) {
+            setBasicUserInfo((oldState) => {
+                if (!oldState) {
+                    return oldState;
+                }
+
+                portfolioAPI
+                    .updateBaseUserInfo({ ...basicUserInfo, aboutMe, id: 1 }, token)
+                    .then(response => console.log(response))
+                    .catch(error => console.log(error));
+
+                return { ...oldState, aboutMe }
+            });
+        }
+    }
+
+    return {
+        basicUserInfo,
+        isLoading,
+        updateAboutMeRequestHandler,
+        updateCarouselImagesHandler,
+        updateEmailRequestHandler,
+        updatePartnersHandler,
+    }
+}
