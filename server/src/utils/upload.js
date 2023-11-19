f249fd9125c3f15b21f336cf8ce74a4f5ee777db
Stoyan Grozdanov <stoyan@Stoyans-MacBook-Pro.local>
2023-11-19 16:09:57 +0200

    fix file upload does not allow different files than pdfs to be uploaded

--- server/src/utils/upload.js
+++ server/src/utils/upload.js
@@ -3,7 +3,7 @@ import multer from 'multer';
 const storage = multer.memoryStorage();
 
 const fileFilter = (req, file, cb) => {
-    if (file.mimetype === 'application/pdf') {
+    if (file.mimetype === 'application/pdf' || file.mimetype.startsWith('image/')) {
         cb(null, true);
     } else {
         cb(null, false);
